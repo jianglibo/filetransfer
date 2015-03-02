de5f6aae1164af08477a2dee31e7611a46a49127
jianglibo <jianglibo@gmail.com>
2015-03-02 13:22:36 +0800

    use locked start

---
+++ src/main/resources/cn/intellijoy/clojure/starter_lock.clj
@@ -0,0 +1,30 @@
+(ns cn.intellijoy.clojure.starter
+  (:require [vertx.core :refer [config deploy-verticle deploy-worker-verticle]]
+            [cn.intellijoy.clojure.sampler :as sampler]
+            [cn.intellijoy.clojure.app-utils :as app-utils]
+            [vertx.logging :as log]))
+
+
+(let [cfg (config)
+      bts (:bytes-to-send cfg)
+      inst-num (atom (:instances cfg 1))
+      bytes-to-send (if (:str-line bts)
+                      bts
+                      (assoc bts :str-line sampler/str-line))]
+
+  ;; start the verticles that make up the app
+  (if (:client cfg)
+    (deploy-verticle "cn/intellijoy/clojure/file_client.clj"
+                   :config (app-utils/sample-upload-data :per-instance-files (:per-instance-files cfg)
+                                                         :total-files (:total-files cfg)
+                                                         :host (:host cfg)
+                                                         :port (:port cfg)
+                                                         :bytes-to-send bytes-to-send)
+                   :instances @inst-num)
+    (do
+      (when (:verify-verticle cfg)
+        (deploy-verticle "cn/intellijoy/clojure/verify_verticle.clj" :config cfg :instances 1))
+      (deploy-verticle "cn/intellijoy/clojure/after_upload.clj" :instances 1)
+      (deploy-verticle "cn/intellijoy/clojure/file_server.clj"
+                       :config cfg
+                       :instances @inst-num))))
